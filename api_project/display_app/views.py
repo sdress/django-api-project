8030600d92793941572286d2568060183c6f8cc8
sdress <sarah.dresser1@gmail.com>
2022-07-24 23:38:37 -0700

    protecting api_key

--- api_project/display_app/views.py
+++ api_project/display_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, HttpResponse
+import requests
+from datetime import datetime
+from django.conf import settings
 
 # Create your views here.
-def index():
-    
+def index(request):
+    key = settings.NASA_API_KEY
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={key}')
+    nasa_data = response.json()
+    return render(request, 'index.html', {
+        'date': datetime.strptime(nasa_data['date'], '%Y-%m-%d').strftime('%m/%d/%Y'),
+        'hdurl': nasa_data['hdurl'],
+        'explanation': nasa_data['explanation']
+    })
