685858da707d040f742ce475ed048b696f1675b5
sdress <sarah.dresser1@gmail.com>
2022-07-25 11:49:49 -0700

    adding API data

--- api_project/display_app/views.py
+++ api_project/display_app/views.py
@@ -8,8 +8,11 @@ def index(request):
     key = settings.NASA_API_KEY
     response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={key}')
     nasa_data = response.json()
+    # print(nasa_data)
     return render(request, 'index.html', {
+        'title': nasa_data['title'],
         'date': datetime.strptime(nasa_data['date'], '%Y-%m-%d').strftime('%m/%d/%Y'),
         'hdurl': nasa_data['hdurl'],
-        'explanation': nasa_data['explanation']
+        'explanation': nasa_data['explanation'],
+        'credit': nasa_data['copyright']
     })
