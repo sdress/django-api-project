6e205b3e6b747ebfcadfe3ce139a76ff43c540c5
sdress <sarah.dresser1@gmail.com>
2022-07-24 23:38:10 -0700

    saving routes

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('display', include('display_app.urls'))
+    path('display/', include('display_app.urls'))
 ]
