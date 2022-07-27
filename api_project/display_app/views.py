5d1c05d9c33abc9da1b3c10403843ce54d5a15b7
sdress <sarah.dresser1@gmail.com>
2022-07-27 10:53:55 -0700

    passing nasa_data to template

--- api_project/display_app/views.py
+++ api_project/display_app/views.py
@@ -3,31 +3,19 @@ import requests
 from datetime import datetime, date, timedelta
 from django.conf import settings
 
+key = settings.NASA_API_KEY
 # Create your views here.
 def index(request):
-    key = settings.NASA_API_KEY
     response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={key}')
     nasa_data = response.json()
-    # print(nasa_data)
-    return render(request, 'index.html', {
-        'title': nasa_data['title'],
-        'date': datetime.strptime(nasa_data['date'], '%Y-%m-%d').strftime('%m/%d/%Y'),
-        'hdurl': nasa_data['hdurl'],
-        'explanation': nasa_data['explanation'],
-        'credit': nasa_data['copyright']
-    })
+    print(nasa_data)
+    if 'copyright' in nasa_data:
+        print(nasa_data['copyright'])
+    return render(request, 'index.html', nasa_data)
 
 def previous(request):
-    key = settings.NASA_API_KEY
     previous = date.today() - timedelta(days=1)
     response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={key}&date={previous}')
     nasa_data = response.json()
     print(nasa_data)
-    return render(request, 'index.html', {
-        'title': nasa_data['title'],
-        'date': datetime.strptime(nasa_data['date'], '%Y-%m-%d').strftime('%m/%d/%Y'),
-        'hdurl': nasa_data['hdurl'],
-        'explanation': nasa_data['explanation'],
-        # TODO: try/except if 'copyright' exists in nasa_data
-        # 'credit': nasa_data['copyright']
-    })
+    return render(request, 'index.html', nasa_data)
